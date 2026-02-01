6123e6e046716cffef988730923a785e540a69ee
mmaric123 <mihailomaric001@gmail.com>
2026-02-01 17:47:11 +0100

    changed names of images

--- source/src/components/whyChoose/WhyChooseV3.tsx
+++ source/src/components/whyChoose/WhyChooseV3.tsx
@@ -15,7 +15,7 @@ const WhyChooseV3 = () => {
                         <div className="col-lg-6 offset-lg-6 pl-60 pl-md-15 pl-xs-15">
                             <h2 className="title"> <br /> Zelena Dvorišta</h2>
                             <p>
-                                Zelena Dvorišta je vaš pouzdan partner u održavanju i razvijanju dvorišta. Naša strast za prirodom i zelenim prostorima osigurava da vaše dvorište bude u najboljem mogućem stanju. Bilo da je reč o redovnom održavanju, uređenju ili specijalizovanim uslugama, naš tim stručnjaka je tu da vam pomogne da ostvarite svoj idealan zeleni kutak.
+                                Zelena Dvorišta je vaš pouzdan partner u održavanju i razvijanju dvorišta. Naša strast za prirodom i zelenim prostorima osigurava da vaše dvorište bude u najboljem mogućem stanju. Bilo da je reč o redovnom održavanju, uređenju ili specijalizovanim uslugama, naš tim stručnjaka je tu da vam pomogne da ostvarite svoj zeleni kutak.
                             </p>
                             <div className="list-grid">
                                 {/* <div className="achivement-content">
